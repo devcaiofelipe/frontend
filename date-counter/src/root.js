c80d5cc5c9849f9109b5bbd9a3ea81f5499dfacd
devcaiofelipe15@gmail.com <devcaiofelipe15@gmail.com>
2024-02-26 07:36:29 -0300

    feat: add new project

--- date-counter/src/root.js
+++ date-counter/src/root.js
@@ -2,9 +2,12 @@ import './root.css';
 import { useState } from 'react';
 
 export default function Root() {
+  const today = new Date();
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
-  const [date, setDate] = useState(new Date());
+  const [range, setRange] = useState(1);
+  const [date, setDate] = useState(today);
+  const [dateAdd, setDateAdd] = useState(today);
 
   const handleIncrementStep = () => {
     setStep(step => step + 1);
@@ -17,14 +20,14 @@ export default function Root() {
   const handleIncrementCount = () => {
     setCount(count => count + step);
     const days = (60 * 60 * 24 * 1000) * step;
-    const newDate = new Date(date.getTime() + days);
+    const newDate = new Date(today.getTime() + days);
     setDate(newDate);
   }
 
   const handleDecrementCount = () => {
     setCount(count => count - step);
     const days = (60 * 60 * 24 * 1000) * step;
-    const newDate = new Date(date.getTime() - days);
+    const newDate = new Date(today.getTime() - days);
     setDate(newDate);
   }
 
@@ -47,19 +50,44 @@ export default function Root() {
     return message.replaceAll(',', '');
   }
 
+
+  // new
+  const handleChange = (e) => {
+    setCount(Number(e.target.value));
+    const days = (60 * 60 * 24 * 1000) * e.target.value;
+    const newDate = new Date(date.getTime() + days);
+    setDateAdd(newDate)
+  }
+
+  const handleIncrement = () => {
+    setCount((count) => count + range)
+    const days = (60 * 60 * 24 * 1000) * (count + range);
+    const newDate = new Date(date.getTime() + days);
+    setDateAdd(newDate)
+  }
+
+  const handleDecrement = () => {
+    setCount((count) => count - range)
+    const days = (60 * 60 * 24 * 1000) * count;
+    const newDate = new Date(date.getTime() - days);
+    setDate(newDate);
+  }
+
+  const handleChangeRange = (e) => {
+    setRange(Number(e.target.value))
+  }
+
   return (
     <div className='root'>
       <div>
-        <button onClick={handleDecrementStep}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={handleIncrementStep}>+</button>
+        <input type='range' step='1' value={range} onChange={handleChangeRange} minLength={0} max={10}/><span>{range}</span>
       </div>
       <div>
-        <button onClick={handleDecrementCount}>-</button>
-        <span>Count: {count}</span>
-        <button onClick={handleIncrementCount}>+</button>
+        <button onClick={handleDecrement}>-</button>
+        <input onChange={handleChange} value={count}/>
+        <button onClick={handleIncrement}>+</button>
       </div>
-      <span>{handleDayMessage(count, date)}</span>
+      <span>{handleDayMessage(count, dateAdd)}</span>
     </div>
   )
 }
